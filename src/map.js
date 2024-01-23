1e7192bd9759f156e85eadfb4888aa9ad79cdfe9
Gloria Lim <glorialimartt@gmail.com>
2024-01-23 10:27:51 -0500

    Add Leaflet-Geoman Drawing toolbar

--- src/map.js
+++ src/map.js
@@ -22,5 +22,20 @@ $(() => {
   // Add scale bar
   L.control.scale().addTo(map);
 
+  // Define Leaflet-Geoman Drawing toolbar options
+  var options = {
+    position: 'topleft', // toolbar position, options are 'topleft', 'topright', 'bottomleft', 'bottomright'
+    drawMarker: true, // adds button to draw markers
+    drawPolyline: true, // adds button to draw a polyline
+    drawRectangle: true, // adds button to draw a rectangle
+    drawPolygon: true, // adds button to draw a polygon
+    drawCircle: true, // adds button to draw a cricle
+    cutPolygon: true, // adds button to cut a hole in a polygon
+    editMode: true, // adds button to toggle edit mode for all layers
+    removalMode: true, // adds a button to remove layers
+  };
+
+  // Add leaflet.pm controls to the map
+  map.pm.addControls(options);
 
 })
